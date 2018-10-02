5df3bcf791ee78a853e68afcf1e028060a32bd05
Roman Bártl <bbartl.roman@gmail.com>
2018-10-02 12:25:37 +0200

    Added simple design for main page

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -5,8 +5,8 @@ namespace App\Presenters;
 
 class HomepagePresenter extends BasePresenter
 {
-	public function renderDefault()
-	{
-		$this->template->anyVariable = 'any value';
-	}
+    public function renderDefault()
+    {
+
+    }
 }
