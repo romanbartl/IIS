f4de4eeaa17f2e8b4178f94c7bf40f123c270e66
Roman Bártl <bbartl.roman@gmail.com>
2018-10-03 09:51:18 +0200

    Added basic pages layouts

---
+++ app/presenters/FestivalsPresenter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 2.10.18
+ * Time: 13:39
+ */
+
+namespace App\Presenters;
+
+
+class FestivalsPresenter extends BasePresenter
+{
+    public function renderDefault()
+    {
+
+    }
+}
---
+++ app/presenters/InterpretsPresenter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 2.10.18
+ * Time: 13:39
+ */
+
+namespace App\Presenters;
+
+
+class InterpretsPresenter extends BasePresenter
+{
+    public function renderDefault()
+    {
+
+    }
+}
