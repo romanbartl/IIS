8676f55e0e32cd7525d3891a3259496f5b2a6024
Roman Bártl <xbartl06@vutbr.cz>
2018-10-16 10:37:43 +0200

    Concerts detail - showing all concerts

--- app/model/ConcertsManager.php
+++ app/model/ConcertsManager.php
@@ -52,4 +52,23 @@ class ConcertsManager
                         LEFT JOIN Concert_has_Interpret AS chi ON chi.idConcert = c.idConcert
                         WHERE chi.idInterpret = ? AND date >= CURDATE()', $interpretId);
     }
+
+
+    /**
+     * @return Nette\Database\ResultSet
+     */
+    public function getAllConcertsWithTickets()
+    {
+        return
+            $this->database->query('SELECT c.idConcert AS idConcert, c.name AS name, c.date AS date, p.name AS placeName, 
+                                        ci.name AS city, COUNT(c.idConcert) AS tickets, i.label AS label 
+                                        FROM Concert AS c 
+                                        LEFT JOIN Place AS p ON p.idPlace = c.idPlace 
+                                        LEFT JOIN City AS ci ON ci.idCity = p.idCity 
+                                        LEFT JOIN Ticket AS t ON t.idConcert = c.idConcert 
+                                        LEFT JOIN Concert_has_Interpret AS chi ON chi.idConcert = c.idConcert 
+                                        LEFT JOIN Interpret AS i ON i.idInterpret = chi.idInterpret 
+                                        WHERE t.bought = 0 AND chi.headliner = 1 
+                                        GROUP BY c.idConcert ORDER BY c.date DESC');
+    }
 }
--- app/presenters/ConcertsPresenter.php
+++ app/presenters/ConcertsPresenter.php
@@ -9,19 +9,43 @@
 namespace App\Presenters;
 
 
+use App\Model\ConcertsManager;
+
 class ConcertsPresenter extends BasePresenter
 {
+    /**
+     * @var ConcertsManager
+     */
+    public $concertsManager;
+
+
+    /**
+     * @var concert id
+     */
     private $concertId;
+
+
+    /**
+     * ConcertsPresenter constructor.
+     * @param ConcertsManager $concertsManager
+     */
+    public function __construct(ConcertsManager $concertsManager)
+    {
+        $this->concertsManager = $concertsManager;
+    }
+
 
     public function renderDefault()
     {
-
+        $this->template->concerts = $this->concertsManager->getAllConcertsWithTickets();
     }
+
 
     public function renderDetail()
     {
         $this->template->concertId = $this->concertId;
     }
+
 
     public function actionDetail($id)
     {
