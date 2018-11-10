b551b8fb359f7e8bbbc40e43f65d0e92bce37624
odehnaltomas <odehnaltomas136@gmail.com>
2018-11-10 18:08:07 +0100

    Added editing of members.

--- app/presenters/InterpretsPresenter.php
+++ app/presenters/InterpretsPresenter.php
@@ -2,11 +2,17 @@
 
 namespace App\Presenters;
 
+use App\Forms\IMemberFormFactory;
+use App\Forms\MemberForm;
+use App\Forms\MemberFormFactory;
 use App\Model\AlbumsManager;
 use App\Model\ConcertsManager;
+use App\Model\DuplicateNameException;
 use App\Model\InterpretsManager;
 use App\Model\MembersManager;
 use App\Model\UserManager;
+use Nette\Application\UI\Multiplier;
+use Nette\Forms\Form;
 
 
 class InterpretsPresenter extends BasePresenter
@@ -44,6 +50,10 @@ class InterpretsPresenter extends BasePresenter
     private $interpretId;
 
 
+    /** @var IMemberFormFactory */
+    private $memberForm;
+
+
     /**
      * InterpretsPresenter constructor.
      * @param InterpretsManager $interpretsManager
@@ -68,9 +78,34 @@ class InterpretsPresenter extends BasePresenter
         $this->template->interprets = $this->interpretsManager->getAllInterprets();
     }
 
+
     public function actionDetail($id)
     {
         $this->interpretId = $id;
+    }
+
+    protected function createComponentMemberForm()
+    {
+        return new Multiplier(function ($idMember, $onSuccess) {
+            $member = $this->membersManager->getMember($idMember);
+            $form = new \Nette\Application\UI\Form;
+            $form->addHidden('idMember', $idMember);
+            $form->addText('name', 'Jméno:')
+                ->setDefaultValue($member->name);
+            $form->addText('surname', 'Příjmení:')
+                ->setDefaultValue($member->surname);
+            $form->addText('birth', 'Datum narození:')
+                ->setType('date')
+                ->setDefaultValue($member->birth->format('Y-m-d'));
+            $form->addSubmit('send', 'Uložit');
+
+            $form->onSuccess[] = function (Form $form, $values) use ($onSuccess) {
+                $this->membersManager->editMember($values);
+                $this->redirect('this');
+            };
+
+            return $form;
+        });
     }
 
     public function renderDetail()
@@ -93,6 +128,14 @@ class InterpretsPresenter extends BasePresenter
                 $this->userManager->removeFavouriteInterpret($idUser, $idInterpret);
             }
             $this->redrawControl('changeFavourite');
+        }
+    }
+
+
+    public function handleDeleteMember($idInterpret, $idMember) {
+        if($this->isAjax()) {
+            $this->membersManager->deleteMemberFromInterpret($idInterpret, $idMember);
+            $this->redrawControl('editMembers');
         }
     }
 }
